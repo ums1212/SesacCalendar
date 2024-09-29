94820eb0d435b3f4bfb20daa39c46684a75f3eb8
ums1212 <voll1212@gmail.com>
2024-09-29 23:51:48 +0900

    upload fragment layout

--- app/src/main/java/com/sesac/sesacscheduler/MainFragment.kt
+++ app/src/main/java/com/sesac/sesacscheduler/MainFragment.kt
@@ -2,14 +2,23 @@ package com.sesac.sesacscheduler
 
 import android.os.Bundle
 import android.view.View
-import androidx.navigation.fragment.findNavController
 import com.sesac.sesacscheduler.databinding.FragmentMainBinding
+import java.util.Calendar
+import java.util.Date
 
 class MainFragment : BaseFragment<FragmentMainBinding>(FragmentMainBinding::inflate) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        findNavController().navigate(R.id.addSchedulerFragment)
+        // 초기 editTextView 힌트 설정
+        Calendar.getInstance().apply {
+            time = Date(binding.calendarView.date)
+            binding.editTextSchedule.hint = "${get(Calendar.MONTH)+1}월 ${get(Calendar.DAY_OF_MONTH)}일에 일정 추가"
+        }
+        // 날짜가 변경될 때마다 hint내용 변경
+        binding.calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            binding.editTextSchedule.hint = "${month+1}월 ${dayOfMonth}일에 일정 추가"
+        }
     }
 
 }
