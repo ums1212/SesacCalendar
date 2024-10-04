0ef5bd84a92c94d222646eb54514e04f58ae7df0
Park MinWook <96559787+mandoofu@users.noreply.github.com>
2024-10-04 13:38:07 +0900

    FEAT : 스케쥴 리스트 레이아웃 추가 및 기능 추가

--- app/src/main/java/com/sesac/sesacscheduler/ScheduleListFragment.kt
+++ app/src/main/java/com/sesac/sesacscheduler/ScheduleListFragment.kt
@@ -1,19 +1,41 @@
 package com.sesac.sesacscheduler
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.navigation.fragment.findNavController
 import com.sesac.sesacscheduler.databinding.FragmentScheduleListBinding
 
 class ScheduleListFragment : BaseFragment<FragmentScheduleListBinding>(FragmentScheduleListBinding::inflate) {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        override lateinit var binding: FragmentScheduleListBinding
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule_list, container, false)
-    }
+        ): View? {
+            binding = FragmentScheduleListBinding.inflate(inflater, container, false)
+            return binding.root
+        }
 
-}
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+
+            // 데이터 준비 (예시)
+            val scheduleList = mutableListOf("일정1", "일정2", "일정3")
+
+            // Adapter 생성
+            val adapter = ArrayAdapter(requireContext(), R.layout.simple_list_item_1, scheduleList)
+
+            // ListView에 Adapter 설정
+            binding.scheduleList.adapter = adapter
+
+            // 데이터 추가 버튼 클릭 이벤트 처리
+            binding.buttonAdd.setOnClickListener {
+                // 데이터 추가
+                scheduleList.add(0, "새로운 일정")
+            }
+        }
+    }
