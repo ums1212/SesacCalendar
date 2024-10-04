ef82fb1fc4a2a3dd68e3b2afc2a5bd13f8572a81
DongSuLee <101309717+dongsu0717@users.noreply.github.com>
2024-10-04 23:00:36 +0900

    [feat]: 스케줄 관리를 위한 ScheduleDAO 추가
    - 추가한 ScheduleDao는 스케줄의 CRUD 작업을 처리
    - 스케줄등록, 스케줄변경, 스케줄삭제, 스케줄가져오기(id, 해당날짜, 해당 월)

--- app/src/main/java/com/sesac/sesacscheduler/dao/ScheduleDAO.kt
+++ app/src/main/java/com/sesac/sesacscheduler/dao/ScheduleDAO.kt
@@ -1,4 +1,36 @@
 package com.sesac.sesacscheduler.dao
 
-class ScheduleDAO {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.sesac.sesacscheduler.model.ScheduleInfo
+
+@Dao
+interface ScheduleDAO {
+
+    @Insert
+    fun insertSchedule(schedule: ScheduleInfo)
+
+    @Update
+    fun updateSchedule(schedule: ScheduleInfo)
+
+    @Delete
+    fun deleteSchedule(schedule: ScheduleInfo)
+
+    @Query("DELETE FROM schedule WHERE scheduleId = :id")
+    fun deleteSchedule(id: Int)
+
+    //id값으로 스케줄 가져오기(수정페이지?)
+    @Query("SELECT * FROM schedule WHERE scheduleId = :id")
+    fun getSchedule(id: Int): ScheduleInfo
+
+    //해당 날짜 스케줄 가져오기
+    @Query("SELECT * FROM schedule WHERE startDate = :date")
+    fun getScheduleDate(date: String): MutableList<ScheduleInfo>
+
+    //해당 월 스케줄 가져오기
+    @Query("SELECT * FROM schedule WHERE startDate LIKE :month || '%'")
+    fun getScheduleMonth(month: String): MutableList<ScheduleInfo>
 }
