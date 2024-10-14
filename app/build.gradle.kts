4b999a085990181999763f0da9ae1f23f4ee723d
DongSuLee <101309717+dongsu0717@users.noreply.github.com>
2024-10-14 17:26:31 +0900

    [feat]: retrofit2,gson 의존성 추가 및 manifest 인터넷 허가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,5 +64,7 @@ dependencies {
 
 
     implementation(libs.bundles.flowbindings)
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
 }
