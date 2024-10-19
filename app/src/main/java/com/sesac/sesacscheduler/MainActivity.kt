a734570002ee844770918cd87557d3fd67aca389
ums1212 <voll1212@gmail.com>
2024-10-19 23:43:02 +0900

    [Feat] 오버레이 알림 기능 추가

--- app/src/main/java/com/sesac/sesacscheduler/MainActivity.kt
+++ app/src/main/java/com/sesac/sesacscheduler/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.sesac.sesacscheduler
 
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.sesac.sesacscheduler.alarm.AlarmScheduler
-import com.sesac.sesacscheduler.alarm.repository.AlarmRepository
-import com.sesac.sesacscheduler.alarm.usecase.AlarmUsecase
 import com.sesac.sesacscheduler.common.logE
-import com.sesac.sesacscheduler.database.ScheduleRoomDatabase
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +26,56 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         logE("mainActivity","onCreate")
+
+        // 오버레이 권한
+        if (!Settings.canDrawOverlays(this)) {
+            requestOverlayPermission()
+        }
+
+        // 위치 권한
+        multiplePermissionsLauncher.launch(permissions)
     }
+
+    private val overlayPermissionLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (Settings.canDrawOverlays(this)) {
+            Log.d("MainActivity", "오버레이 권한이 허용되었습니다.")
+        } else {
+            Log.d("MainActivity", "오버레이 권한이 거부되었습니다.")
+        }
+    }
+
+    // 오버레이 권한 요청
+    private fun requestOverlayPermission() {
+        val intent = Intent(
+            Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+            Uri.parse("package:$packageName")
+        )
+        overlayPermissionLauncher.launch(intent)
+    }
+
+    private val multiplePermissionsLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+        var result = true
+        permissions.entries.forEach { (_, isGranted) ->
+            when {
+                isGranted -> {
+                    // 개별 권한이 승인된 경우 처리할 작업
+                }
+                !isGranted -> {
+                    // 권한이 거부된 경우 처리할 작업
+                    result = false
+                }
+                else -> {
+                    // 사용자가 "다시 묻지 않음"을 선택한 경우 처리할 작업
+                    result = false
+                }
+            }
+        }
+    }
+
+    private val permissions = arrayOf(
+        ACCESS_FINE_LOCATION,
+        ACCESS_COARSE_LOCATION
+    )
 
 }
