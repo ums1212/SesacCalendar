08d034696ac35083b673200f3c7fb077422d4f60
DongSuLee <101309717+dongsu0717@users.noreply.github.com>
2024-10-20 14:13:45 +0900

    [refactor]: ScheduleViewModel 중복코드 리팩토리

--- app/src/main/java/com/sesac/sesacscheduler/viewmodel/ScheduleViewModel.kt
+++ app/src/main/java/com/sesac/sesacscheduler/viewmodel/ScheduleViewModel.kt
@@ -2,7 +2,6 @@ package com.sesac.sesacscheduler.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.navigation.NavController
 import com.sesac.sesacscheduler.alarm.AlarmScheduler
 import com.sesac.sesacscheduler.alarm.repository.AlarmRepository
 import com.sesac.sesacscheduler.alarm.usecase.AlarmUsecase
@@ -10,12 +9,12 @@ import com.sesac.sesacscheduler.common.ScheduleResult
 import com.sesac.sesacscheduler.common.SchedulerApplication
 import com.sesac.sesacscheduler.common.logE
 import com.sesac.sesacscheduler.common.toastShort
-import com.sesac.sesacscheduler.dao.ScheduleDAO
 import com.sesac.sesacscheduler.database.ScheduleRoomDatabase
 import com.sesac.sesacscheduler.model.ScheduleInfo
 import com.sesac.sesacscheduler.repo.ScheduleRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
@@ -44,97 +43,80 @@ class ScheduleViewModel : ViewModel() {
 
     private val ioDispatchers = CoroutineScope(Dispatchers.IO)
 
+    private fun <T> handleResult(result: ScheduleResult<T>, successMessage: String) {
+        when (result) {
+            is ScheduleResult.Success -> {
+                viewModelScope.launch {
+                    toastShort(successMessage)
+                    logE("성공", result.toString())
+                }
+            }
+            is ScheduleResult.Loading -> {
+                viewModelScope.launch {
+                    logE("로딩", result.toString())
+                }
+            }
+            is ScheduleResult.RoomDBError -> {
+                logE("DB 에러", result.exception.toString())
+            }
+            else -> {
+                logE("예상치 못한 상태", result.toString())
+            }
+        }
+    }
+    private fun <T> fetchAndUpdateState(
+        fetch: suspend () -> Flow<ScheduleResult<T>>,
+        stateFlow: MutableStateFlow<ScheduleResult<T>>
+    ) {
+        viewModelScope.launch {
+            withContext(ioDispatchers.coroutineContext) {
+                fetch().collectLatest {
+                    stateFlow.value = it
+                }
+            }
+        }
+    }
+
     fun insertSchedule(newSchedule: ScheduleInfo) {
         viewModelScope.launch {
             withContext(ioDispatchers.coroutineContext) {
                 repository.insertSchedule(newSchedule).collectLatest {
-                    when (it) {
-                        is ScheduleResult.Success -> {
-                            viewModelScope.launch {
-                                toastShort("스케줄 저장")
-                                logE("스케줄 저장(성공)", it.toString())
-                                AlarmScheduler(SchedulerApplication.getSchedulerApplication(), AlarmUsecase(AlarmRepository(scheduleDAO)))
-                                    .scheduleAlarmsForAppointments()
-                            }
-                        }
-                        is ScheduleResult.Loading -> {
-                            viewModelScope.launch {
-                                toastShort("스케줄 저장중...")
-                                logE("스케줄 저장(로딩)", it.toString())
-                            }
-                        }
-
-                        is ScheduleResult.RoomDBError -> {
-                            logE("스케줄 저장(DB에러)", it.exception.toString())
-                        }
-
-                        else -> {
-                            logE("스케줄 저장(너가 왜뜨니?)", it.toString())
-                        }
+                    handleResult(it, "스케줄 저장")
+                    if (it is ScheduleResult.Success) {
+                        AlarmScheduler(SchedulerApplication.getSchedulerApplication(), AlarmUsecase(AlarmRepository(scheduleDAO)))
+                            .scheduleAlarmsForAppointments()
                     }
+                }
+            }
+        }
+    }
+
+    fun updateSchedule(updatedSchedule: ScheduleInfo) {
+        viewModelScope.launch {
+            withContext(ioDispatchers.coroutineContext) {
+                repository.updateSchedule(updatedSchedule).collectLatest {
+                    handleResult(it, "스케줄 수정 완료")
                 }
             }
         }
     }
 
     fun getSchedule(id: Int) {
-        viewModelScope.launch {
-            withContext(ioDispatchers.coroutineContext) {
-                repository.getSchedule(id).collectLatest {
-                    _schedule.value = it
-                }
-            }
-        }
+        fetchAndUpdateState({ repository.getSchedule(id) }, _schedule)
+    }
+
+    fun findAllSchedule() {
+        fetchAndUpdateState({ repository.findAllSchedule() }, _scheduleList)
+    }
+
+    fun findScheduleByMonth(month: String) {
+        fetchAndUpdateState({ repository.getScheduleMonth(month) }, _scheduleList)
     }
 
     fun deleteSchedule(id: Int) {
         viewModelScope.launch {
             withContext(ioDispatchers.coroutineContext) {
-                repository.deleteSchedule(id)/*.collectLatest {
-                    _schedule.value = it
-            }*/
-            }
-        }
-    }
-    fun findAllSchedule() = viewModelScope.launch {
-        withContext(ioDispatchers.coroutineContext) {
-            repository.findAllSchedule().collectLatest {
-                _scheduleList.value = it
-            }
-        }
-    }
-    fun findScheduleByMonth(month: String) = viewModelScope.launch {
-        withContext(ioDispatchers.coroutineContext) {
-            repository.getScheduleMonth(month).collectLatest {
-                _scheduleList.value = it
-            }
-        }
-    }
-    fun updateSchedule(updatedSchedule: ScheduleInfo) {
-        viewModelScope.launch {
-            withContext(ioDispatchers.coroutineContext) {
-                repository.updateSchedule(updatedSchedule).collectLatest {
-                    when (it) {
-                        is ScheduleResult.Success -> {
-                            viewModelScope.launch {
-                                toastShort("스케줄 수정 완료")
-                                logE("스케줄 수정(성공)", it.toString())
-
-                            }
-                        }
-                        is ScheduleResult.Loading -> {
-                            logE("스케줄 수정(로딩)", it.toString())
-                        }
-
-                        is ScheduleResult.RoomDBError -> {
-                            logE("스케줄 수정(DB에러)", it.exception.toString())
-                        }
-
-                        else -> {
-                            logE("스케줄 수정(너가 왜뜨니?)", it.toString())
-                        }
-                    }
-                }
+                repository.deleteSchedule(id)
             }
         }
     }
